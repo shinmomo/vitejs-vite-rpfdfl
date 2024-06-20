2caf3b71075692fb8e91049a58e88310ec65b28b
shinmomo <shinmomo19450902@gmail.com>
2024-06-20 18:12:20 +0900

    スプレッド

--- main.js
+++ main.js
@@ -39,10 +39,65 @@
 // console.log(func3(5,10));
 
 
-const func3 = (num1, num2) => ({
-    hoge: num1,
-    huga: num2,
-});
-console.log(func3(10, 20));
+// const func3 = (num1, num2) => ({
+//     hoge: num1,
+//     huga: num2,
+// });
+// console.log(func3(10, 20));
 
+// const myProfile = {
+//     name: "もも";
+//     age: 27
+// };
+
+
+// const sayHello = (name = "名無し") => console.log(`こんにちは!${name}さん！`);
+// sayHello();
+
+// const myProfile = {
+//     age: 31
+// };
+// const { age, name = "ゲスト" } = myProfile;
+// console.log(age);
+// console.log(name);
+
+// const name ="もも";
+// const age = 27;
+
+// //オブジェクトの省略記法
+// const myProfile ={
+//     name,
+//     age
+// };
+// console.log(myProfile);
+
+// //スプレッド構文
+// const arr1 = [1, 2];
+// // console.log(arr1);  
+// // console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// // sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// const arr2 = [1,2,3,4,5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// console.log(arr6);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+const arr8 = [...arr4];
+console.log(arr8);
+
+arr4[0] = 20;
+console.log(arr8);
 
