f846adc4bcb3a7a71efb35958e27eb6dfa314bc7
shinmomo <shinmomo19450902@gmail.com>
2024-06-20 17:42:27 +0900

    アロー関数

--- main.js
+++ main.js
@@ -1,2 +1,31 @@
-// const func3 = (num1, num2) =>  num1 + num2;
-// console.log(func3(10,20));
+//アロー関数
+
+//従来
+// function func1(str) {
+//     return str;
+// }
+
+// const func1 = function(str) {
+//     return str;
+// };
+// console.log(func1("func1です"));
+
+//アロー関数(引数)=>{関数}
+// const func2 = (str) => {
+//     return str;
+// };
+
+// console.log(func2("func2です"));
+
+//引数が1つなら()を消してよい
+// const func2 = str => {
+//     return str;
+// };
+// console.log(func2("func2です"));
+
+//評価項目が1個だけ、かつ引数が1個
+const func2 = str => str;
+console.log(func2("func2です"));
+
+const arrowDoubleFunc = a => 2 * a;
+console.log(arrowDoubleFunc(5));
