4b6641b36584f8fd29869a474d59e7f8328f968a
shinmomo <shinmomo19450902@gmail.com>
2024-06-20 17:49:55 +0900

    アロー関数

--- main.js
+++ main.js
@@ -24,8 +24,25 @@
 // console.log(func2("func2です"));
 
 //評価項目が1個だけ、かつ引数が1個
-const func2 = str => str;
-console.log(func2("func2です"));
+// const func2 = str => str;
+// console.log(func2("func2です"));
 
-const arrowDoubleFunc = a => 2 * a;
-console.log(arrowDoubleFunc(5));
+// const arrowDoubleFunc = a => 2 * a;
+// console.log(arrowDoubleFunc(5));
+
+//練習
+// const func3 = (num1, num2) => {
+//     return num1 + num2;
+// };
+
+// const func3 = (num1,num2) => num1 + num2;
+// console.log(func3(5,10));
+
+
+const func3 = (num1, num2) => ({
+    hoge: num1,
+    huga: num2,
+});
+console.log(func3(10, 20));
+
+
